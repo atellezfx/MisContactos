5a3f77a374f9738024a9e1761dbdadc0a65782f8
alejandro <alejandro.tellez@gmail.com>
2014-03-31 17:44:23 -0600

    Commit inicial del proyecto

---
+++ app/src/main/java/com/acme/miscontactos/MainActivity.java
@@ -0,0 +1,15 @@
+package com.acme.miscontactos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
