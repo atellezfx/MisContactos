59715e63da86131d5c0024cec40ce8afb686c45d
alejandro <alejandro.tellez@gmail.com>
2014-03-31 18:06:25 -0600

    Primera parte de interfaz grafica

--- app/src/main/java/com/acme/miscontactos/MainActivity.java
+++ app/src/main/java/com/acme/miscontactos/MainActivity.java
@@ -2,14 +2,52 @@ package com.acme.miscontactos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.acme.miscontactos.util.TextChangedListener;
 
 
 public class MainActivity extends Activity {
+
+    private EditText txtNombre, txtTelefono, txtEmail, txtDireccion;
+    private Button btnAgregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inicializarComponentesUI();
     }
 
+    private void inicializarComponentesUI() {
+        txtNombre = (EditText) findViewById(R.id.cmpNombre);
+        txtTelefono = (EditText) findViewById(R.id.cmpTelefono);
+        txtEmail = (EditText) findViewById(R.id.cmpEmail);
+        txtDireccion = (EditText) findViewById(R.id.cmpDireccion);
+        txtNombre.addTextChangedListener(new TextChangedListener() {
+            @Override
+            public void onTextChanged(CharSequence seq, int i, int i2, int i3) {
+                btnAgregar = (Button) findViewById(R.id.btnAgregar);
+                btnAgregar.setEnabled(!seq.toString().trim().isEmpty());
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        String mesg = String.format("%s ha sido agregado a la lista!", txtNombre.getText());
+        Toast.makeText(this, mesg, Toast.LENGTH_SHORT).show();
+        btnAgregar.setEnabled(false);
+        limpiarCampos();
+    }
+
+    private void limpiarCampos() {
+        txtNombre.getText().clear();
+        txtTelefono.getText().clear();
+        txtEmail.getText().clear();
+        txtDireccion.getText().clear();
+        txtNombre.requestFocus();
+    }
 }
---
+++ app/src/main/java/com/acme/miscontactos/util/TextChangedListener.java
@@ -0,0 +1,25 @@
+package com.acme.miscontactos.util;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by alejandro on 3/31/14.
+ */
+public class TextChangedListener implements TextWatcher {
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+}
